c0c995f4f43a3db75be7a0b13e0f45e3fb2b469d
Craig Myles <cr@igmyles.com>
2018-11-23 13:51:43 +1100

    Added node and created README

---
+++ README.md
@@ -0,0 +1,48 @@
+# Instant Tools
+
+CLI for assisting with common Instant-Feedback related tasks.
+
+## Features
+
+### init
+
+Clone a template repository and optionally install dependencies.
+
+## Installation
+
+```bash
+$ npm i instant-tools -g
+```
+
+## Example usage
+
+```bash
+$ instant init react my-react-app -i
+```
+
+## Development
+
+To contribute to the codebase, clone the repository and install the dependencies:
+
+```bash
+git clone https://github.com/JSJInvestments/instant-tools.git
+cd instant-tools && npm i
+```
+
+Modify `src/index.js`.
+
+Increment the [semver](https://semver.org/) version in package.json.
+
+Publish to npm
+
+```bash
+$ npm publish
+```
+
+## Running Tests
+
+```bash
+npm run test
+```
+
+## License
--- src/index.js
+++ src/index.js
@@ -6,27 +6,32 @@ import chalk from 'chalk';
 import pjson from '../package.json';
 import { spawn } from 'child_process';
 
-const installReact = async (name, cmd) => {
-  const repoUrl = config.get('react.url');
+const createApplication = async (type, name, cmd) => {
+  const repoUrl = config.get(`${type}.url`);
 
-  console.log(chalk.bold.magenta('Initialising new React application'));
+  console.log(chalk.bold.magenta(`Initialising new ${type} application`));
   console.log(`${chalk.bold.cyan('Cloning')} ${repoUrl}...`);
 
   try {
-    await clone(repoUrl, name);
+    clone(repoUrl, name, () => {
+      if (cmd.install) {
+        console.log(chalk.bold.cyan('Installing dependencies...'));
+        const deps = spawn('npm', ['install'], {
+          cwd: name,
+          stdio: 'inherit',
+        });
 
-    if (cmd.install) {
-      console.log(chalk.bold.cyan('Installing dependencies...'));
-      const deps = spawn('npm', ['install'], {
-        cwd: name,
-        stdio: 'inherit',
-      });
-      deps.on('close', code => {
+        deps.on('error', () => {
+          console.log(chalk.bold.red(error));
+        });
+
+        deps.on('close', code => {
+          console.log(chalk.bold.cyan('Application created!'));
+        });
+      } else {
         console.log(chalk.bold.cyan('Application created!'));
-      });
-    } else {
-      console.log(chalk.bold.cyan('Application created!'));
-    }
+      }
+    });
   } catch (error) {
     console.error(error);
   }
@@ -41,8 +46,12 @@ program
     console.log(chalk.bold.cyan(pjson.name) + ` version ${pjson.version}`);
     console.log();
 
-    if (type === 'react') {
-      installReact(name, cmd);
+    if (type === 'react' || type === 'node') {
+      createApplication(type, name, cmd);
+    } else {
+      throw new Error(
+        'Unrecognised application type. Options include `react` or `node`'
+      );
     }
   });
 
