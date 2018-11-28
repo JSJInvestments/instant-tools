150c4d0bda9a33d6ecd8e540e76f4c36ed693de9
Craig Myles <cr@igmyles.com>
2018-11-29 10:58:05 +1100

    Upgraded code

--- src/index.js
+++ src/index.js
@@ -6,19 +6,56 @@ import chalk from 'chalk';
 import pjson from '../package.json';
 import { spawn } from 'child_process';
 import pify from 'pify';
+import fs from 'fs-extra';
+// import path from 'path';
 
-const cloneRepo = pify((url, name, callback) => {
-  try {
+const print = {
+  start: () => {
+    console.log();
+    console.log(chalk.bold.cyan(pjson.name) + ` version ${pjson.version}`);
+    console.log();
+  },
+  create: (type, dir) => {
+    // Creating a new React app in [/Users/craig/dev/sandbox/create-react-app-v2]green.
+    console.log(
+      `${chalk.bold.magenta(
+        `Creating a new ${type} app in`
+      )} ${chalk.bold.green(process.cwd() + '/' + dir)}`
+    );
+  },
+  clone: url => {
     console.log(`${chalk.bold.cyan('Cloning')} ${url}...`);
-    clone(url, name, callback);
+  },
+  install: () => {
+    console.log(chalk.bold.cyan('Installing dependencies...'));
+  },
+  created: dir => {
+    console.log(
+      `${chalk.bold.cyan('Application')} ${chalk.bold.green(
+        dir
+      )} ${chalk.bold.cyan('created!')}`
+    );
+    console.log();
+  },
+};
+
+const cloneRepo = async (url, dir) => {
+  try {
+    print.clone(url);
+
+    const exists = await fs.pathExists(dir);
+    if (exists) {
+      throw `Directory ${dir} already exists!`;
+    } else {
+      await pify(clone)(url, dir);
+    }
   } catch (error) {
-    console.log(chalk.bold.red(error));
+    throw error;
   }
-});
+};
 
 const installDependencies = pify(async (dir, callback) => {
   try {
-    console.log(chalk.bold.cyan('Installing dependencies...'));
     const deps = spawn('npm', ['install'], {
       cwd: dir,
       stdio: 'inherit',
@@ -30,41 +67,52 @@ const installDependencies = pify(async (dir, callback) => {
 
     deps.on('close', callback);
   } catch (error) {
-    console.log(chalk.bold.red(error));
+    throw error;
   }
 });
 
-const createApplication = async (type, name, cmd) => {
-  console.log(chalk.bold.magenta(`Initialising new ${type} application`));
-
+const createApplication = async (type, dir, cmd) => {
   try {
+    print.create(type, dir);
+
     const repoUrl = config.get(`${type}.url`);
-    await cloneRepo(repoUrl, name);
+    await cloneRepo(repoUrl, dir);
     if (cmd.install) {
-      await installDependencies(name);
+      await installDependencies(dir);
     }
-    console.log(chalk.bold.cyan('Application created!'));
+
+    print.created(dir);
   } catch (error) {
-    console.log(chalk.bold.red(error));
+    throw error;
   }
 };
 
-program
-  .version('0.1.0')
-  .command('init <type> <name>')
-  .option('-i, --install', 'Install dependences')
-  .action(async (type, name, cmd) => {
-    console.log();
-    console.log(chalk.bold.cyan(pjson.name) + ` version ${pjson.version}`);
-    console.log();
+export default async function execute() {
+  try {
+    let type;
+    let name;
+    let cmd;
+
+    program
+      .version('0.1.0')
+      .command('create <type> <name>')
+      .option('-i, --install', 'Install dependences')
+      .action((t, n, c) => {
+        type = t;
+        name = n;
+        cmd = c;
+      });
+
+    program.parse(process.argv);
+
+    print.start();
 
     if (type === 'react' || type === 'node') {
-      createApplication(type, name, cmd);
+      await createApplication(type, name, cmd);
     } else {
-      throw new Error(
-        'Unrecognised application type. Options include `react` or `node`'
-      );
+      throw 'Unrecognised application type. Options include `react` or `node`';
     }
-  });
-
-program.parse(process.argv);
+  } catch (error) {
+    console.log(chalk.bold.red(error));
+  }
+}
